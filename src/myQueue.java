e9f0f9f4b39de45781a209d7a885b22d2481a1b0
Mahdokht Af <mmafravi@gmail.com>
2020-04-20 11:39:06 -0600

    created assignment for labw13: added src folder with 3 starter classes and PDF instructions

---
+++ src/myQueue.java
@@ -0,0 +1,17 @@
+public class myQueue {
+	private String[] data;
+	private int head;
+	private int numElements;
+	
+	/* ********** Constructors ********** */
+	public myQueue(int size) {
+		
+	}
+	
+	public myQueue() {
+		
+	}
+	
+	/* ********** Operations ********** */
+	//your functions go here
+}
---
+++ src/myStack.java
@@ -0,0 +1,16 @@
+public class myStack {
+	private String[] data;
+	private int top;
+	
+	/* ********** Constructors ********** */
+	public myStack(int size) {
+		
+	}
+	
+	public myStack() {
+		
+	}
+	
+	/* ********** Operations ********** */
+	//your functions go here
+}
